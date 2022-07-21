0101d1e043a575ea54a5d0f2f91c3ddfe62ca6b8
Sunairaa <sunairashahzeb@gmail.com>
2022-07-21 12:41:49 +0200

    added iteration 7

--- src/movies.js
+++ src/movies.js
@@ -1,3 +1,78 @@
+
+const movies2 = [
+    {
+      title: 'The Shawshank Redemption',
+      year: 1994,
+      director: 'Frank Darabont',
+      duration: '2h 22min',
+      genre: ['Crime', 'Drama'],
+      score: 9.3
+    },
+    {
+      title: 'The Godfather',
+      year: 1972,
+      director: 'Francis Ford Coppola',
+      duration: '2h 55min',
+      genre: ['Crime', 'Drama'],
+      score: 9.2
+    },
+    {
+      title: 'The Godfather: Part II',
+      year: 1974,
+      director: 'Francis Ford Coppola',
+      duration: '3h 22min',
+      genre: ['Crime', 'Drama'],
+      score: 9
+    },
+    {
+      title: 'The Dark Knight',
+      year: 2008,
+      director: 'Christopher Nolan',
+      duration: '2h 32min',
+      genre: ['Action', 'Crime', 'Drama', 'Thriller'],
+      score: 9
+    },
+    {
+      title: '12 Angry Men',
+      year: 1957,
+      director: 'Sidney Lumet',
+      duration: '1h 36min',
+      genre: ['Crime', 'Drama'],
+      score: 8.9
+    },
+    {
+      title: 'Schindler"s List',
+      year: 1993,
+      director: 'Steven Spielberg',
+      duration: '3h 15min',
+      genre: ['Biography', 'Drama', 'History'],
+      score: 8.9
+    },
+    {
+      title: 'Pulp Fiction',
+      year: 1994,
+      director: 'Quentin Tarantino',
+      duration: '2h 34min',
+      genre: ['Crime', 'Drama'],
+      score: 8.9
+    },
+    {
+      title: 'The Lord of the Rings: The Return of the King',
+      year: 2003,
+      director: 'Peter Jackson',
+      duration: '3h 21min',
+      genre: ['Adventure', 'Drama', 'Fantasy'],
+      score: 8.9
+    },
+    {
+      title: 'Il buono, il brutto, il cattivo',
+      year: 1966,
+      director: 'Sergio Leone',
+      duration: '3h 2min',
+      genre: ['Western'],
+      score: 8.9
+    }
+]
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -22,7 +97,7 @@ function howManyMovies(moviesArray) {
     }
     return count;
 }
-console.log(howManyMovies(movies2));
+// console.log(howManyMovies(movies2));
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
@@ -73,7 +148,7 @@ function orderByYear(moviesArray) {
     });
     return copyMovieArray;
 }
-console.log(orderByYear(movies2));
+// console.log(orderByYear(movies2));
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {
@@ -93,7 +168,29 @@ function orderAlphabetically(moviesArray) {
 }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) {
+    const updatedArray = moviesArray.map(function(movie){
+        movie.duration = durationToMins(movie.duration);
+        return movie;
+
+    })
+
+    return updatedArray;
+}
+
+
+// console.log(turnHoursToMinutes([{ duration: '5h 41min' }])[0].duration)
+
+function durationToMins(timeStr) {
+    timeArray = timeStr.split(' ');
+    for (let i = 0; i < timeArray.length; i++) {
+        if (timeArray.length === 1) {
+            return Number(timeArray[0].replace('h',"")) * 60;
+        }
+        return Number(timeArray[0].replace('h',"")) * 60 + Number(timeArray[1].replace('min',""));
+    }
+}
+console.log(turnHoursToMinutes(movies2))
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
